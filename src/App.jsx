0399199c58e97b05b34f6efd534adc59c530137c
Vincde <145802178+Vincde@users.noreply.github.com>
2025-06-12 15:04:48 +0200

    Add props in Score component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import Score from './components/Score';
 
 import './App.css'
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   
+  const handleScore = (element) => {
+    setScore(element);
+  }
+
+  const handleBestScore = (element) => {
+    setBestScore(element);
+  }
 
   return(
     <div>
-      <Score></Score>
+      <Score score={score} bestScore={bestScore}></Score>
+      <Cards></Cards>
     </div>
   )
 }
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,14 @@
-function Score() {
+function Score({score, bestScore}) {
     return(
-        <div>
-            <p>Score: </p>
-            <p>Best Score: </p>
-        </div>
+        <>
+            <div>
+                <h1>The Memory game</h1>
+                <p>Get points by clicking on an image but don't click on any more than once!</p>
+            </div>
+            <div>
+                <p>Score: {score}</p>
+                <p>Best Score: {bestScore}</p>
+            </div>
+        </>
     )
 }
