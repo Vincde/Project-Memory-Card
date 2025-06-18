44c88dec432c912cb29baad8a1f876cad18b5a53
Vincde <145802178+Vincde@users.noreply.github.com>
2025-06-18 20:40:41 +0200

    Feat: Add fetchPokemons method into a different file, implementation needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Score from './components/Score';
+import fetchPokemons from './components/fetchPokemon';
 
 import './App.css'
 
@@ -8,26 +9,13 @@ let didInit = false;   // purpose: not making react remounting twice per fetched
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     if(!didInit){
     didInit = true;
-
-
-    const randomNumber = Math.floor(Math.random() * 1302 + 1);
-    const pokemons = [];
-    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${randomNumber}&limit=12`) 
-    .then((result) => result.json()) // the first fetch returns urls of pokemons
-    .then((element) => {
-      for(const [id, fetchedPokemon] of element.results.entries()) {  // iterate over the array of pokemons
-        fetch(fetchedPokemon.url)   // fetch the single pokemon
-        .then((result) => result.json())
-        .then((pokemon) => pokemons.push({id: id, value: pokemon.sprites.front_default})) // save the url of pokemon sprite in pokemon
-        .then(() => console.log(pokemons));
-      }});
-
-
-
+    const firstPokemons = fetchPokemons();
+    setPokemons([...firstPokemons]);
     }
   }, []); 
 
