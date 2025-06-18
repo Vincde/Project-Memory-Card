e0ba7834c3397c5b884b573efe2f5fa2b682d18a
Vincde <145802178+Vincde@users.noreply.github.com>
2025-06-18 11:11:19 +0200

    Feat: Implementing the whole fetching pokemon effect
    The useEffect at the start of the App runs one time. It fetches the link with a random number. Since it returns an array of all the pokemon urls, a second fetch is needed to take the front_sprite of the image, that gets pushed into the pokemon array that will be used later

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import Score from './components/Score';
 
 import './App.css'
 
+let didInit = false;   // purpose: not making react remounting twice per fetched element
+
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
   useEffect(() => {
+    if(!didInit){
+    didInit = true;
+
+
     const randomNumber = Math.floor(Math.random() * 1302 + 1);
     const pokemons = [];
-    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${randomNumber}&limit=12`)
-    .then((result) => result.json())
-    .then((element) => pokemons.push(element.url));
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${randomNumber}&limit=12`) 
+    .then((result) => result.json()) // the first fetch returns urls of pokemons
+    .then((element) => {
+      for(const [id, fetchedPokemon] of element.results.entries()) {  // iterate over the array of pokemons
+        fetch(fetchedPokemon.url)   // fetch the single pokemon
+        .then((result) => result.json())
+        .then((pokemon) => pokemons.push({id: id, value: pokemon.sprites.front_default})) // save the url of pokemon sprite in pokemon
+        .then(() => console.log(pokemons));
+      }});
+
+
+
+    }
   }, []); 
 
 
 
-  
   
   const handleScore = (element) => {
     setScore(element);
