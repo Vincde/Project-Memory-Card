95cfd4ca8f63fc6c741320fec9af1114e186d246
Vincde <145802178+Vincde@users.noreply.github.com>
2025-06-20 16:20:49 +0200

    Add randomizeCards function and shuffleArray function

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,19 @@
 export default function Cards({changeScore, changeBestScore, pokemons, handlePokemons}) {
 
     function randomizeCards() {
+        const newArray = shuffleArray(pokemons);
+        handlePokemons([...newArray]);
+    }
 
+
+    function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+            // Generate a random index between 0 and i
+            const j = Math.floor(Math.random() * (i + 1));
+            // Swap elements array[i] and array[j]
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
     }
 
 
