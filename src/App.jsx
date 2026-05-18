7fbe7d2fb5ce590f23312d3d0de65788ba5437ae
Vincde <145802178+Vincde@users.noreply.github.com>
2026-05-18 14:55:51 +0200

    Add primitive design notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { useState, useEffect } from 'react';
+
+/* 
+  Component design:
+    - An introduction page, with title and score
+      - Score has two things: current score and best score
+    
+    - Central part of the page:
+      - The cards
 
 
-import './App.css'
+    - Current score state
+    - Best score state
 
+    -Cards
+      each card is clickable.
+      three possibilities:
+        - the card is new
+        - the card is already clicked
+*/
 
 function App() {
 }
