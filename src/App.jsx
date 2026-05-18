d87136d720109036aabea131de12fbe767d8dbdd
Vincde <145802178+Vincde@users.noreply.github.com>
2026-05-18 14:47:26 +0200

    Reset code to start a new implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,10 @@
 import { useState, useEffect } from 'react';
-import Score from './components/Score';
-import Cards from './components/Cards';
-import fetchPokemons from './components/fetchPokemon';
+
 
 import './App.css'
 
-let didInit = false;   // purpose: not making react remounting twice per fetched element
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [pokemons, setPokemons] = useState([]);
-
-  useEffect(() => {
-    if(!didInit){
-    didInit = true;
-    fetchPokemons()
-    .then((array) => setPokemons([...array]));
-    }
-  }, []); 
-
-
-  const handlePokemons = (pokemons) => {
-    setPokemons([...pokemons]);
-  }
-  
-  const handleScore = (element) => {
-    setScore(element);
-  }
-
-  const handleBestScore = (element) => {
-    setBestScore(element);
-  }
-
-  return(
-    <>
-      <Score score={score} bestScore={bestScore}></Score>
-      <Cards score={score}
-             changeScore={handleScore}
-             bestScore={bestScore}
-             changeBestScore={handleBestScore}
-             pokemons={pokemons}
-             handlePokemons={handlePokemons}
-      ></Cards>
-   </>
-
-  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-/* 
-  Reasoning first: 
-  -what the UI will look like : various cards (probably 5 rows and 4-5 columns) with score in the upper part of page
-  -What are the inputs: clicking on the cards triggers the mechanism
-  -What are the outputs: reset the point counter, or add point
-
-*/
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
